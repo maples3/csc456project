1d08d0ad25e16090215c7649f078383c67c7e28e
Anthony <anthonymapes3@gmail.com>
2019-11-30 22:46:38 -0500

    Minor tweaks for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import CardList from './components/CardList';
 import Login from './components/Login';
 import { store } from './store/store';
@@ -8,14 +8,14 @@ import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
     <Provider store={store}>
         <div className="App">
           <Route exact path='/' component={Login} />
           <Route exact path='/list' component={CardList} />
         </div>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
     );
 
 }
